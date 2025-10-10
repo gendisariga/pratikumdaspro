6016e5c8f0596052d109538448fb90bfcee93dfe
gendis kumala ariga <gendiskumalariga@gmail.com>
2025-10-10 10:31:10 +0700

    wifi11

---
+++ jobsheet5/wificampus11.java
@@ -0,0 +1,30 @@
+package pratikumdaspro.jobsheet5;
+
+import java.util.Scanner;
+
+public class wificampus11 {
+    public static void main(String[] args) {
+     Scanner endis = new Scanner (System.in);
+        String jenisPengguna;
+        int JumlahSKS;
+        System.out.print("Jenis Pengguna: ");
+        jenisPengguna = endis.nextLine();
+
+        if (jenisPengguna.equalsIgnoreCase ("Dosen")){
+            System.out.println("Akses Wifi diberikan (dosen)");
+           }else if (jenisPengguna.equalsIgnoreCase("Mahasiswa")){
+            System.out.print("Input jumlah SKS: ");
+            JumlahSKS = endis.nextInt();
+            if (JumlahSKS >= 12){
+            System.out.println("Akses Wifi diberikan (mahasiswa aktif)");
+           }else{
+           System.out.println("Akses ditolak, SKS Kurang dari 12");
+           }
+        }else { 
+        System.out.print("Akses ditolak");    
+        }
+        
+       
+    }
+    
+}
