2423f5d3013e54da48e877fcad205c748efbbe87
gendis kumala ariga <gendiskumalariga@gmail.com>
2025-10-10 09:33:11 +0700

    percobaan1

---
+++ jobsheet5/ifcetakKRS11.java
@@ -0,0 +1,23 @@
+
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifcetakKRS11 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("CETAK KRS SIAKAD");
+         System.out.println("apakah ukt sudah lunas?( true/ false)");
+         boolean uktlunas= sc.nextBoolean();
+         if (uktlunas) {
+            System.out.println("pembayaran ukt terverivikasi");
+            System.out.println("silahkan cetak krs dan minta tanda tabfab dpa");
+            
+            
+         }
+     }
+   
+    
+}
+
+
